97780d8accda57401ae4284f94fac2ae26e6a32b
Thomas Gakumu <gakumuthomas@gmail.com>
2026-04-23 21:40:47 +0300

    Open the script on the js in the directory

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+const openWho = document.querySelector('.btn--who');
+
+const overlay = document.querySelector('.overlay');
+
+const closeWho = document.querySelector('.close--who');
+
+console.log(openWho);
+
+const dog = [2, 3];
+console.log(dog);
