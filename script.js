e7326d9e01bcc4bc2a30c406a8bad09848ed5ee2
Thomas Gakumu <gakumuthomas@gmail.com>
2026-04-27 00:18:47 +0300

    Done manipulation of DOM

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+const who = document.querySelector('.thomas');
+const overlay = document.querySelector('.overlay');
+const btnCloseWho = document.querySelector('.close--who');
+const btnOpenWho = document.querySelector('.btn--who');
+
+btnOpenWho.addEventListener('click', function () {
+  who.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+});
+
+btnCloseWho.addEventListener('click', function () {
+  console.log('btn clicked');
+  who.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+  who.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
